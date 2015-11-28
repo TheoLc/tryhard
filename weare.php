b65370f7bc7e7388b435257f4f165498aef54292
Thilak93 <Thilakzan-93@hotmail.fr>
2015-11-28 17:22:10 +0100

    toto

--- weare.php
+++ weare.php
@@ -7,10 +7,18 @@ include("index.php");
 <title>Nous sommes</title>
 </head>
 <body>
-	<div id="#">
+	<div class="decal">
+		<h3>Nous sommes des étudiants de l'ETNA qui avons conçu ce site E-commerce de Castelis pour un projet hackaton en une semaine</h3>
+		<hr color="white">
 		<h3>Noutchat TCHATAT</h3>
+		<a href="mailto:tchata_n@etna-alternance.net"><img src="img/tchata_n.jpg" title="cliquer dessus pour m'écrire"/></a>
+		<p><a class="linkMail" href="mailto:tchata_n@etna-alternance.net">tchata_n@etna-alternance.net</a></p>
 		<h3>Thilakzan KANAGARATNAM</h3>
+		<a href="mailto:kanaga_t@etna-alternance.net"><img src="img/kanaga_t.jpg" title="cliquer dessus pour m'écrire"/></a>
+		<p><a class="linkMail" href="mailto:kanaga_t@etna-alternance.net">kanaga_t@etna-alternance.net</a></p>
 		<h3>Théo LO CASTRO</h3>
+		<a href="mailto:locast_t@etna-alternance.net"><img src="img/locast_t.jpg" title="cliquer dessus pour m'écrire"/></a>
+		<p><a class="linkMail" href="mailto:locast_t@etna-alternance.net">locast_t@etna-alternance.net</a></p>
 	</div>
 </body>
 </html>
